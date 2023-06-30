88e56a720ef102193af0077e153615b2eddda55f
julzdoug <jschwarzzh@hotmail.com>
2023-06-30 08:31:38 +0200

    error message login away

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -156,20 +156,7 @@ const handleLinkedInSignup = async () => {
       } catch (error) {
         alert(error.message);
       } 
-        try {
-    loading.value = true
-    const { error } = await supabase.auth.signInWithOtp({
-      email: email.value,
-    })
-    if (error) throw error
-    alert('Check your email for the login link!')
-  } catch (error) {
-    if (error instanceof Error) {
-      alert(error.message)
-    }
-  } finally {
-    loading.value = false
-  }
+ 
       
     };
 
