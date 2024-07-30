a090d7b75c2bcaec48047b32c1b94d0c375b261c
julzdoug <jschwarzzh@hotmail.com>
2024-07-30 21:44:16 +0200

    vue login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
+  import { supabase } from '../utils/supabase'
+  const todos = ref([])
 
+  async function getTodos() {
+    const { data } = await supabase.from('todos').select()
+    todos.value = data
+  }
+
+  onMounted(() => {
+    getTodos()
+  })
 </script>
-
 <template>
 
   <div class="max-w-7xl p-10 m-auto">
